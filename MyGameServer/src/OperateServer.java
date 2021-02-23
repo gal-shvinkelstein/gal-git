e3037223a346d77984947bb5f1a5134392a0c3da
gal.shvinkelstein <gal.gmn@gmail.com>
2021-02-23 11:39:52 +0200

    updates

--- MyGameServer/src/OperateServer.java
+++ MyGameServer/src/OperateServer.java
@@ -2,27 +2,94 @@ import java.net.ServerSocket;
 import java.nio.CharBuffer;
 import java.util.HashMap;
 import java.util.function.Function;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
-public class OperateServer {
-    public class MainServer
+public class OperateServer
+{
+    private HashMap<Integer, ClientData> m_client_list;
+
+    private ServerSocketChannel serverSocketChannel;
+    private Reactor[] reactors;
+
+    private Selector selector;
+    private int noOfReactors;
+
+    public OperateServer(String host, int port, int noOfWorkerThreads)
     {
-        public void HandleReq()
-        {
+        try {
+            selector = Selector.open();
+            reactors = new Reactor[noOfWorkerThreads];
+            this.noOfReactors = noOfWorkerThreads;
+            for (int i = 0; i < noOfWorkerThreads; i++) {
+                reactors[i] = new Reactor();
+                Thread thread = new Thread(reactors[i]);
+                thread.start();
+            }
 
+            serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.bind(new InetSocketAddress(host, 9000));
+            serverSocketChannel.configureBlocking(false);
+
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        } catch (IOException e) {
+            //handle exceptions
         }
-
-        public void HandleRep()
-        {
-
-        }
-
-        private HashMap<Integer, ClientData> m_client_list;
-        ServerSocket m_server_socket;
     }
 
-    static void main(String args[])
+    public void start() throws IOException
     {
-        //NIO reactor implementation
 
+        int i = 0;
+
+        while (true)
+        {
+
+            int readyChannels = selector.select();
+            if (readyChannels == 0)
+                continue;
+
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+
+            Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+
+            while (keyIterator.hasNext())
+            {
+
+                SelectionKey key = keyIterator.next();
+
+                if (key.isAcceptable())
+                {
+
+                    ServerSocketChannel serverSocket = (ServerSocketChannel) key.channel();
+                    SocketChannel socket = serverSocket.accept();
+                    reactors[i % noOfReactors].addChannel(socket);
+                    i++;
+
+                }
+
+                keyIterator.remove();
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        OperateServer acceptor = new OperateServer("localhost", 9000, 4);
+        try {
+            acceptor.start();
+        } catch (IOException e) {
+            // e.printStackTrace();
+        }
     }
 }
+
+
+
---
+++ MyGameServer/src/Reactor.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Reactor implements Runnable {
+
+    private Queue queue = new ConcurrentLinkedQueue<SocketChannel>();
+
+    private Selector selector;
+
+    public Reactor() {
+        try {
+            this.selector = Selector.open();
+        } catch (IOException e) {
+            // e.printStackTrace();
+        }
+    }
+
+    public void addChannel(SocketChannel socketChannel) {
+        queue.add(socketChannel);
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+
+            try {
+                SocketChannel socketChannel = (SocketChannel) queue.poll();
+                if (socketChannel == null)
+                    continue;
+
+                socketChannel.configureBlocking(false);
+                socketChannel.register(selector, SelectionKey.OP_READ);
+
+                int readyChannels = selector.select();
+
+                if (readyChannels == 0)
+                    continue;
+
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+
+                Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+
+                while (keyIterator.hasNext()) {
+
+                    SelectionKey key = keyIterator.next();
+
+                    if (key.isReadable()) {
+                        // Read the channel
+
+                    }
+
+                    keyIterator.remove();
+                }
+            } catch (IOException e) {
+                //handle IOExceptions
+            }
+        }
+    }
+}
