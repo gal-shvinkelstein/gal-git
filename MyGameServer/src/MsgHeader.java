e2d40f1e32c780c6ed32f0bce0d87dae34431cf4
gal.shvinkelstein <gal.gmn@gmail.com>
2021-02-23 15:19:18 +0200

    update

--- MyGameServer/src/MsgHeader.java
+++ MyGameServer/src/MsgHeader.java
@@ -1,7 +1,7 @@
 import java.nio.CharBuffer;
 
 public class MsgHeader {
-    public int req_type;
+    public ReqType req_type;
     public int req_len;
     public int usr_Id;
     public int game_id;
---
+++ MyGameServer/src/ReqType.java
@@ -0,0 +1,14 @@
+public enum ReqType
+{
+    Register,
+    Login,
+    Logout,
+    CreateLobby,
+    JoinLobby,
+    LeaveLobby,
+    Purchase,
+    StartGame,
+    LeaveGame,
+    JoinGame,
+    RestartGame
+}
