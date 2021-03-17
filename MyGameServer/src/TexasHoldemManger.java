db32f97d238fb8dc266bcb5b02508d9c9b54d3f9
gal.shvinkelstein <gal.gmn@gmail.com>
2021-03-17 10:58:01 +0200

    .

--- MyGameServer/src/TexasHoldemManger.java
+++ MyGameServer/src/TexasHoldemManger.java
@@ -2,9 +2,8 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class TexasHoldemManger implements IGamesManager{
-    public TexasHoldemManger(ClientData opener)
-    {
+public class TexasHoldemManger implements IGamesManager {
+    public TexasHoldemManger(ClientData opener) {
         pots = new ArrayList<>();
         joined_num = 0;
         player_turn_index = 0;
@@ -17,8 +16,9 @@ public class TexasHoldemManger implements IGamesManager{
         next_turn = new Vector<>();
         opener.curr_game_score = 2500; // default buy in
         opener.curr_status = 1; // wait for next hand
-        m_active_players.put(opener.id,opener);
+        m_active_players.put(opener.id, opener);
         board = new ArrayList<>();
+        doActionFactory = new DoAction();
 
         next_turn.set(joined_num++, opener);
 
@@ -27,11 +27,10 @@ public class TexasHoldemManger implements IGamesManager{
         game_deck.shuffle();
 
 
-
     }
+
     @Override
-    public boolean JoinGame(ClientData joiner)
-    {
+    public boolean JoinGame(ClientData joiner) {
         joiner.curr_game_score = 2500;
         joiner.curr_status = 1;
         m_active_players.put(joiner.id, joiner);
@@ -39,16 +38,16 @@ public class TexasHoldemManger implements IGamesManager{
 
         return true;
     }
+
     @Override
-    public void RestartGame()
-    {
+    public void RestartGame() {
         StartNewRound();
     }
+
     @Override
-    public void LeaveGame(ClientData leaver)
-    {
+    public void LeaveGame(ClientData leaver) {
         //cash out...
-        if(game_step != 0) {
+        if (game_step != 0) {
             MsgHeader ret = new MsgHeader();
             ret.usr_Id = leaver.id;
             ret.req_type = ReqType.LeaveGame;
@@ -57,14 +56,14 @@ public class TexasHoldemManger implements IGamesManager{
 
         m_active_players.remove(leaver.id);
     }
+
     @Override
-    public MsgHeader Next(MsgHeader last_move)
-    {
+    public MsgHeader Next(MsgHeader last_move) {
         MsgHeader ret = new MsgHeader();
         ret.req_type = ReqType.PlayNext;
         ret.usr_Id = next_turn.get(player_turn_index).id;
 
-        if(game_step == 0) // dealing cards
+        if (game_step == 0) // dealing cards
         {
             StartNewRound();
             ret.buffer = DealNextHand();
@@ -73,34 +72,31 @@ public class TexasHoldemManger implements IGamesManager{
             player_turn_index = (player_turn_index + 1) % next_turn.size();
             //todo: saving each player hand data
         }
-        if(game_step == 1) // placing first bet
+        if (game_step == 1) // placing first bet
         {
-            if (massage_or_action == 0)
-            {
+            if (massage_or_action == 0) {
                 ret.game_status = 100;
                 //small blind
-                if(next_turn.get(player_turn_index).id == next_turn.get(SmallBlindIndex).id)
-                {
+                if (next_turn.get(player_turn_index).id == next_turn.get(SmallBlindIndex).id) {
                     ret.buffer = Action.Type.SmallBlinds;
                     ret.quantity_param = SmallBlind;
-                    new Action(Action.Type.SmallBlinds,SmallBlind);
-                }
-                else {
+                    Action small = doActionFactory.GetAction(Action.Type.SmallBlinds);
+                    small.Do(m_active_players.get(last_move.usr_Id), SmallBlind);
+                } else {
                     //big blind
                     ret.buffer = Action.Type.BigBlinds;
                     ret.quantity_param = SmallBlind * 2;
-                    new Action(Action.Type.BigBlinds, SmallBlind * 2);
+                    Action big = doActionFactory.GetAction(Action.Type.BigBlinds);
+                    big.Do(m_active_players.get(last_move.usr_Id), SmallBlind * 2);
 
                     massage_or_action = 2;
                 }
                 player_turn_index = (player_turn_index + 1) % next_turn.size();
-            }
-            else {
+            } else {
                 ret = BettingRound(last_move);
             }
 
-        }
-        else if (game_step == 2) // flop
+        } else if (game_step == 2) // flop
         {
             // creating string table;
             game_deck.deal(); // burned card
@@ -111,73 +107,61 @@ public class TexasHoldemManger implements IGamesManager{
             ret.game_manger_msg = board.stream().map(Objects::toString).collect(Collectors.joining(", "));
             ret.game_status = 200;
             game_step++;
-        }
-        else if(game_step == 3 || game_step == 5 || game_step == 7) // bet round after cards open
+        } else if (game_step == 3 || game_step == 5 || game_step == 7) // bet round after cards open
         {
             ret = BettingRound(last_move);
-        }
-        else if(game_step == 4 || game_step == 6) // deal turn or river
+        } else if (game_step == 4 || game_step == 6) // deal turn or river
         {
             ret = DealOneToBoard();
-        }
-        else
-        {
+        } else {
 
         }
 
-        if(in_hand_counter == curr_in_hand)
-        {
+        if (in_hand_counter == curr_in_hand) {
             game_step = (game_step + 1) % NumOfGameSteps;
             in_hand_counter = 0;
             player_turn_index = next_turn.indexOf(pots.get(0).contributors.stream().findAny());
-            if(game_step == 0)
-            {
+            if (game_step == 0) {
                 ret = RoundResult();
                 ++SmallBlindIndex;
             }
         }
 
-         // update according to contributors
+        // update according to contributors
         return ret;
     }
 
-    private MsgHeader BettingRound(MsgHeader last_move)
-    {
+    private MsgHeader BettingRound(MsgHeader last_move) {
         MsgHeader ret = new MsgHeader();
-        if(massage_or_action == 1)
-        {
+        if (massage_or_action == 1) {
             //asking next turn to take an action
             ret.game_status = 100; // 100 - take an action, client read instruction
             int curr_bet = 0;
-            for(Pot pot : pots)
-            {
+            for (Pot pot : pots) {
                 curr_bet += pot.GetCurrBet();
             }
             ret.game_manger_msg = "Curr bet is:  " + curr_bet;
             massage_or_action = 2;
             ++in_hand_counter;
             player_turn_index = (player_turn_index + 1) % next_turn.size();
-        }
-        else
-        {
+        } else {
             //check last move action + update pot data
             Action.Type type = (Action.Type) last_move.buffer;
-            new Action(type,last_move.quantity_param);
+            Action action = doActionFactory.GetAction(type);
+            action.Do(m_active_players.get(last_move.usr_Id), last_move.quantity_param);
             //update all players
             ret.game_status = 200; // client read massage
             ret.game_manger_msg = pots.stream().map(Object::toString).collect(Collectors.joining(", "));
             massage_or_action = 1;
         }
         curr_in_hand = pots.get(0).contributors.size();
-        if(curr_in_hand == 1)
-        {
+        if (curr_in_hand == 1) {
             ret = RoundResult();
         }
         return ret;
     }
 
-    private MsgHeader DealOneToBoard()
-    {
+    private MsgHeader DealOneToBoard() {
         MsgHeader ret = new MsgHeader();
         game_deck.deal(); // burned card
         board.add(game_deck.deal());
@@ -195,17 +179,13 @@ public class TexasHoldemManger implements IGamesManager{
         in_hand_counter = 0;
         ret.game_status = 2;
         int total_pot = 0;
-        for(Pot pot : pots)
-        {
+        for (Pot pot : pots) {
             total_pot += pot.GetPotVal();
         }
 
-        if(curr_in_hand > 1)
-        {
+        if (curr_in_hand > 1) {
             //calculate results
-        }
-        else
-        {
+        } else {
             Optional<ClientData> winner = pots.get(0).contributors.stream().findAny();
 
             ret.game_manger_msg = "pot winner is: " + winner.get().id + " pot value is: " + total_pot;
@@ -214,28 +194,25 @@ public class TexasHoldemManger implements IGamesManager{
         }
 
 
-
         return ret;
     }
-    private List<Card> DealNextHand()
-    {
+
+    private List<Card> DealNextHand() {
         List<Card> new_hand = game_deck.deal(2);
         return new_hand;
     }
-    private void StartNewRound()
-    {
+
+    private void StartNewRound() {
         player_turn_index = SmallBlindIndex;
         pots.forEach((n) -> n.Clear());
         int counter = next_turn.size();
         int i;
-        for(i = SmallBlindIndex; i < next_turn.size(); ++i)
-        {
+        for (i = SmallBlindIndex; i < next_turn.size(); ++i) {
             --counter;
             pots.get(0).contributors.add(next_turn.get(i));
         }
         i = 0;
-        while (counter != 0)
-        {
+        while (counter != 0) {
             pots.get(0).contributors.add(next_turn.get(i));
             ++i;
             --counter;
@@ -245,18 +222,17 @@ public class TexasHoldemManger implements IGamesManager{
     }
 
     @Override
-    public int GetMaxPlayers()
-    {
+    public int GetMaxPlayers() {
         return max_players;
     }
+
     @Override
-    public int GetCurrActiveNumOfPlayers()
-    {
+    public int GetCurrActiveNumOfPlayers() {
         return m_active_players.size();
     }
+
     @Override
-    public HashMap<Integer, ClientData > GetForBroadcast()
-    {
+    public HashMap<Integer, ClientData> GetForBroadcast() {
         return m_active_players;
     }
 
@@ -269,35 +245,34 @@ public class TexasHoldemManger implements IGamesManager{
     private int joined_num;
     private Deck game_deck;
     private final int max_players = 8;
-    private HashMap<Integer, ClientData > m_active_players;
+    private HashMap<Integer, ClientData> m_active_players;
     private int in_hand_counter;
     private int curr_in_hand;
     private final List<Pot> pots;
     private int massage_or_action;
     private final List<Card> board;
+    private DoAction doActionFactory;
 
-
-
-    public static class Pot
-    {
-        public Pot(int initial_val)
-        {
+    public static class Pot {
+        public Pot(int initial_val) {
             this.curr_bet = initial_val;
             contributors = new HashSet<>();
         }
+
         public void Clear() {
             pot_val = 0;
             contributors.clear();
         }
+
         public void addContributor(ClientData player) {
             contributors.add(player);
         }
-        public int GetCurrBet()
-        {
+
+        public int GetCurrBet() {
             return curr_bet;
         }
-        public int GetPotVal()
-        {
+
+        public int GetPotVal() {
             return pot_val;
         }
 
@@ -310,6 +285,7 @@ public class TexasHoldemManger implements IGamesManager{
             contributors.add(player);
             return pot;
         }
+
         @Override
         public String toString() {
             StringBuilder sb = new StringBuilder();
@@ -330,19 +306,15 @@ public class TexasHoldemManger implements IGamesManager{
             sb.append(')');
             return sb.toString();
         }
+
         public final Set<ClientData> contributors;
         private int pot_val;
         private int curr_bet;
 
     }
-    public static class Action
-    {
-        public Action(Type act,int bet)
-        {
-            //calling the proper action
-        }
 
-        public enum Type{
+    public interface Action {
+        enum Type {
             SmallBlinds,
             BigBlinds,
             Call,
@@ -352,16 +324,69 @@ public class TexasHoldemManger implements IGamesManager{
             Continue //update contributors next_turn in case of leaving
         }
 
-        private void Small(int small_blind)
-        {
+        public void Do(ClientData player,int bet);
+
+    }
+
+    public class DoAction {
+        public Action GetAction(Action.Type act) {
+            return switch (act) {
+                case SmallBlinds -> new SmallA();
+                case BigBlinds -> new BigA();
+                case Call -> new CallA();
+                case Bet -> new BetA();
+                case AllIn -> new AllInA();
+                case Fold -> new FoldA();
+                case Continue -> new ContinueA();
+            };
+        }
+    }
+
+    public class SmallA implements Action {
+        public void Do(ClientData player, int bet) {
 
         }
-        private void Big(int big_blind)
-        {
+    }
+
+    public class BigA implements Action {
+        public void Do(ClientData player, int bet) {
+
+        }
+    }
+
+    public class CallA implements Action {
+        public void Do(ClientData player, int bet) {
+
+        }
+    }
+
+    public class BetA implements Action {
+        public void Do(ClientData player, int bet) {
 
         }
 
     }
 
+    public class AllInA implements Action {
+        public void Do(ClientData player, int bet) {
 
+        }
+
+    }
+
+    public class FoldA implements Action {
+        public void Do(ClientData player, int bet) {
+
+        }
+
+    }
+
+    public class ContinueA implements Action {
+        public void Do(ClientData player, int bet) {
+
+        }
+
+    }
 }
+
+
